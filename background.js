d621a5817d63e1ec5cafb2438dc4770361bcc634
Salih <salih_borucu@hotmail.com>
2020-07-25 23:53:30 +0100

    Functionality done, all operational. Requires error handling, refactoring, tidying and design tweaks.

--- background.js
+++ background.js
@@ -2,24 +2,30 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
     if (msg.from === 'content' && msg.subject === 'showPageAction') {
         if (msg.lead) {
             var bkg = chrome.extension.getBackgroundPage();
-            bkg.console.log(msg.lead);
-            const options = {
-                method: 'POST',
-                body: JSON.stringify(msg.lead),
-            };
 
-            fetch('http://journeycrm.test/api/incomplete-leads', options)
-                .then((response) => {
-                    bkg.console.log(response.text());
-                    bkg.console.log('done1');
-                })
-                .then((jsonObject) => {
-                    bkg.console.log('done2');
-                    bkg.console.log(jsonObject);
-                })
-                .catch((error) => {
-                    bkg.console.error(`didn't work`);
-                });
+            chrome.storage.sync.get(['userEmail', 'userPassword'], function (result) {
+                let obj = msg.lead;
+                obj['userEmail'] = result.userEmail;
+                obj['userPassword'] = result.userPassword;
+
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(obj),
+                };
+
+                fetch('http://journeycrm.test/api/incomplete-leads', options)
+                    .then((response) => {
+                        bkg.console.log(response.text());
+                        bkg.console.log('done1');
+                    })
+                    .then((jsonObject) => {
+                        bkg.console.log('done2');
+                        bkg.console.log(jsonObject);
+                    })
+                    .catch((error) => {
+                        bkg.console.error(`didn't work`);
+                    });
+            });
         }
         chrome.pageAction.show(sender.tab.id);
     }
--- popup.js
+++ popup.js
@@ -1,4 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.sync.get(['userEmail', 'userPassword'], function (result) {
+        if (result.userEmail && result.userPassword) {
+            document.getElementById('login-component').innerHTML = '';
+            document.getElementById('login-component').innerHTML = `
+                        <h1>Welcome!</h1>
+                        <button class="btn btn-outline-light w-100 mb-3" id="sign-out-button">Log Out</button>`;
+            document.getElementById('sign-out-button').addEventListener('click', function () {
+                chrome.storage.sync.set({ userEmail: null, userPassword: null }, function () {
+                    document.getElementById('login-component').innerHTML = '';
+                    document.getElementById('login-component').innerHTML = `
+                                        <input type="text" class="form-control mb-1" id="user-email" placeholder="example@example.com" />
+                <input type="password" class="form-control mb-1" id="user-password"/>
+                <button class="btn btn-outline-light w-100 mb-3" id="sign-in-button">Sign In</button>`;
+                });
+            });
+        } else {
+            document.getElementById('login-component').innerHTML = '';
+            document.getElementById('login-component').innerHTML = `
+                                        <input type="text" class="form-control mb-1" id="user-email" placeholder="example@example.com" />
+                <input type="password" class="form-control mb-1" id="user-password"/>
+                <button class="btn btn-outline-light w-100 mb-3" id="sign-in-button">Sign In</button>`;
+            document.getElementById('sign-in-button').addEventListener('click', function () {
+                let userEmail = document.getElementById('user-email').value;
+                let userPassword = document.getElementById('user-password').value;
+
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify({ userEmail, userPassword }),
+                };
+
+                fetch('http://journeycrm.test/api/login', options)
+                    .then((response) => {
+                        response.text().then((result) => {
+                            // if (result === 'done') {
+                            document.getElementById('login-component').innerHTML = '';
+                            document.getElementById('login-component').innerHTML = `
+                        <h1>Welcome!</h1>
+                        <button class="btn btn-outline-light w-100 mb-3" id="sign-out-button">Log Out</button>`;
+                            document.getElementById('sign-out-button').addEventListener('click', function () {
+                                console.log('signing out');
+                            });
+                            chrome.storage.sync.set({ userEmail, userPassword }, function () {});
+                        });
+                    })
+                    .catch((error) => {
+                        console.error(`API error: Couldn't send data over.`);
+                    });
+            });
+        }
+    });
+
     document.getElementById('sign-in-button').addEventListener('click', function () {
         let userEmail = document.getElementById('user-email').value;
         let userPassword = document.getElementById('user-password').value;
@@ -11,10 +62,17 @@ document.addEventListener('DOMContentLoaded', function () {
         fetch('http://journeycrm.test/api/login', options)
             .then((response) => {
                 response.text().then((result) => {
-                    if(result === 'done'){
-                        console.log(result)
-                    }
-                        console.log(result);
+                    // if (result === 'done') {
+                    document.getElementById('login-component').innerHTML = '';
+                    document.getElementById('login-component').innerHTML = `
+                        <h1>Welcome!</h1>
+                        <button class="btn btn-outline-light w-100 mb-3" id="sign-out-button">Log Out</button>`;
+                    document.getElementById('sign-out-button').addEventListener('click', function () {
+                        console.log('signing out');
+                    });
+                    chrome.storage.sync.set({ userEmail, userPassword }, function () {});
+                    // }
+                    // console.log(result);
                 });
             })
             .catch((error) => {
